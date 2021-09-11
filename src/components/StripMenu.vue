b101631bdd4787a4ed8f52f956f80a719731c91c
Matix-Media <support@matix-media.net>
2021-09-11 19:31:40 +0200

    Implemented strip menu

--- src/components/StripMenu.vue
+++ src/components/StripMenu.vue
@@ -1,6 +1,12 @@
 <template>
-    <ul class="strip-menu">
-        <li v-for="(item, index) in menu" :key="index" @click="click(item)">
+    <ul class="strip-menu" v-click-outside="close">
+        <li
+            v-for="(item, index) in menu"
+            :key="index"
+            @click="click(item)"
+            @mouseenter="hover(item)"
+            :class="{ open: item.open }"
+        >
             {{ item.text }}
 
             <strip-menu-popout v-if="item.children && item.open" :menu="item.children" />
@@ -10,6 +16,7 @@
 
 <script>
 import StripMenuPopout from "./StripMenuPopout.vue";
+import ClickOutside from "vue-click-outside";
 
 export default {
     name: "StripMenu",
@@ -20,13 +27,66 @@ export default {
             required: true,
         },
     },
+    directives: {
+        ClickOutside,
+    },
     methods: {
         click(item) {
             item.open = !item.open;
+            this.$forceUpdate();
+        },
+        hover(item) {
+            var anyOpen = false;
+            for (let i of this.menu) {
+                if (i.open === true) {
+                    anyOpen = true;
+                    break;
+                }
+            }
+            if (!anyOpen) return;
+
+            for (let i of this.menu) i.open = false;
+            item.open = true;
+            this.$forceUpdate();
+        },
+        close() {
+            var anyOpen = false;
+            for (let i of this.menu)
+                if (i.open === true) {
+                    anyOpen = true;
+                    break;
+                }
+            if (!anyOpen) return;
+
+            for (let i of this.menu) i.open = false;
             this.$forceUpdate();
         },
     },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.strip-menu {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    font-size: 0.9rem;
+
+    > li {
+        display: inline-block;
+        padding: 0.5rem 0.5rem;
+        cursor: pointer;
+        transition: color 0.2s;
+
+        &:hover,
+        &.open {
+            color: rgba(255, 255, 255, 0.9);
+        }
+        &.open {
+            background-color: rgb(48, 61, 68);
+            border-radius: 3px 3px 0 0;
+            transition: color 0.2s, background-color 0.2s;
+        }
+    }
+}
+</style>
--- src/components/StripMenuPopout.vue
+++ src/components/StripMenuPopout.vue
@@ -1,18 +1,35 @@
 <template>
-    <ul class="strip-menu-popout">
+    <ul class="strip-menu-popout" :class="{ 'sub-menu': isSubmenu }">
         <li
             class="strip-menu-item"
+            :class="{ open: item.open }"
             v-for="(item, index) of menu"
             :key="index"
             @click.prevent="click($event, item)"
             @mouseenter="hover(item)"
         >
-            <span v-if="item.icon">
-                {{ item.icon }}
-            </span>
-            {{ item.text }}
+            <div class="content" v-if="!item.seperator">
+                <div>
+                    <span class="icon" v-if="item.icon">
+                        {{ item.icon }}
+                    </span>
 
-            <strip-menu-popout v-if="item.children" v-show="item.open" :menu="item.children" />
+                    <span class="text">
+                        {{ item.text }}
+                    </span>
+                </div>
+
+                <i class="fas fa-chevron-right expandable" v-if="item.children"></i>
+            </div>
+
+            <span class="seperator" v-if="item.seperator"></span>
+
+            <strip-menu-popout
+                v-if="item.children"
+                v-show="item.open"
+                :menu="item.children"
+                :isSubmenu="true"
+            />
         </li>
     </ul>
 </template>
@@ -25,6 +42,11 @@ export default {
             type: Array,
             required: true,
         },
+        isSubmenu: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
     },
     methods: {
         click(e, item) {
@@ -35,19 +57,70 @@ export default {
             }
         },
         hover(item) {
-            if (!item.children) return;
-
             for (let i of this.menu) {
                 i.open = false;
             }
-            item.open = true;
+            if (item.children) item.open = true;
             this.$forceUpdate();
         },
-        closeAll() {
-            
-        }
     },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.strip-menu-popout {
+    cursor: default;
+    animation: fadeIn 0.2s;
+    padding: 0;
+    margin: 0;
+    margin-left: -0.5rem;
+    list-style-type: none;
+    background-color: rgb(48, 61, 68);
+    padding: 0.5rem 0;
+    box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+        rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+    border-radius: 3px;
+    line-height: 1.2em;
+    color: #fff;
+    position: absolute;
+
+    li {
+        font-weight: 300;
+        position: relative;
+        cursor: pointer;
+        color: #fff;
+        padding: 0.3rem 1em;
+        cursor: pointer;
+        user-select: none;
+
+        &:hover,
+        &.open {
+            background-color: rgba(255, 255, 255, 0.1);
+        }
+
+        .content {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .icon {
+            }
+
+            .text {
+                margin-right: 1rem;
+            }
+
+            .expandable {
+                font-size: 0.8rem;
+                color: rgba(255, 255, 255, 0.8);
+            }
+        }
+
+        .strip-menu-popout {
+            top: -0.5rem;
+            left: calc(100% + 0.7rem);
+            width: max-content;
+        }
+    }
+}
+</style>
