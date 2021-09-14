42e2ccfd0835022257cd303f382d7b85fb9b0805
Matix-Media <support@matix-media.net>
2021-09-14 12:05:55 +0200

    Added overflow y

--- src/components/FullList.vue
+++ src/components/FullList.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="full-list">
-        <div class="header" :class="{'has-content': $slots.header}">
-            {{title}}
+        <div class="header" :class="{ 'has-content': $slots.header }">
+            {{ title }}
             <div class="header-content" v-if="$slots.header">
                 <slot name="header"></slot>
             </div>
         </div>
-        <div class="content" :style="{padding: padding, overflow: overflowHidden ? 'hidden' : 'visible'}">
+        <div
+            class="content"
+            :style="{
+                padding: padding,
+                overflow: overflowHidden ? 'hidden' : 'visible',
+                'overflow-y': overflowY,
+                height: height,
+            }"
+        >
             <slot />
         </div>
     </div>
@@ -18,19 +26,25 @@ export default {
     props: {
         title: String,
         padding: {
-            default: ".5rem"
+            default: ".5rem",
         },
         overflowHidden: {
-            default: true
-        }
-    }
-}
+            default: true,
+        },
+        overflowY: {
+            default: "hidden",
+        },
+        height: {
+            default: "auto",
+        },
+    },
+};
 </script>
 
 <style scoped>
 div.content {
     margin-top: 1rem;
-    padding: .5rem;
+    padding: 0.5rem;
     backdrop-filter: blur(30px);
     background-color: rgba(255, 255, 255, 0.02);
 }
@@ -45,7 +59,7 @@ div.content {
 }
 
 .header::after {
-    content: '';
+    content: "";
     border-top: 1px solid rgba(69, 90, 100, 0.65);
     margin-left: 1rem;
     flex: 1 0 20px;
