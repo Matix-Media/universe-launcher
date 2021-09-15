0cded8abb48ab5ceabaac5462f8f3dca5502e6a5
Matix-Media <support@matix-media.net>
2021-09-15 14:46:33 +0200

    Added Ace Editor library with helpers

---
+++ src/components/AceEditor.vue
@@ -0,0 +1,41 @@
+<template>
+    <ace-editor
+        :value="content"
+        @input="$emit('change', $event)"
+        @init="init"
+        :lang="language"
+        theme="monokai"
+    />
+</template>
+
+<script>
+import AceEditor from "vue2-ace-editor";
+import "brace/theme/monokai";
+
+export default {
+    components: { AceEditor },
+    props: {
+        content: {
+            type: String,
+            default: "",
+        },
+        language: {
+            type: String,
+            default: "text",
+        },
+    },
+    model: {
+        prop: "content",
+        event: "change",
+    },
+    methods: {
+        init(editor) {
+            editor.renderer.setScrollMargin(6, 0);
+            editor.setOptions({
+                wrap: true,
+                scrollPastEnd: 0.5,
+            });
+        },
+    },
+};
+</script>
