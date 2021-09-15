7d49af43e396ed5afafbb9780d13a38fb8faf560
Matix-Media <support@matix-media.net>
2021-09-15 16:12:29 +0200

    Added custom styling

--- src/components/AceEditor.vue
+++ src/components/AceEditor.vue
@@ -33,9 +33,31 @@ export default {
             editor.renderer.setScrollMargin(6, 0);
             editor.setOptions({
                 wrap: true,
-                scrollPastEnd: 0.5,
+                scrollPastEnd: 0.9,
+                vScrollBarAlwaysVisible: true,
             });
         },
     },
 };
 </script>
+
+<style lang="scss">
+.ace_editor {
+    .ace_gutter-cell.ace_error {
+        background-image: url("../assets/images/editor/code/error.png");
+    }
+
+    .ace_tooltip {
+        z-index: 999;
+        color: #fff;
+        background-color: rgb(48, 61, 68);
+        box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 8px 0px;
+        border-radius: 3px;
+        border: none;
+        max-height: 15rem;
+        font-family: Roboto;
+        overflow: auto;
+        padding: 0.5rem 0.6rem;
+    }
+}
+</style>
