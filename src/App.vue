ce1bc505e4cdb372926f00cf80b2c5d288d5e522
Matix-Media <support@matix-media.net>
2021-09-24 18:21:35 +0200

    Removed array prototype modification

--- src/App.vue
+++ src/App.vue
@@ -107,21 +107,6 @@ export default {
         Modal,
         MarkdownItVue,
     },
-    created() {
-        Array.prototype.remove = function() {
-            var what,
-                a = arguments,
-                L = a.length,
-                ax;
-            while (L && this.length) {
-                what = a[--L];
-                while ((ax = this.indexOf(what)) !== -1) {
-                    this.splice(ax, 1);
-                }
-            }
-            return this;
-        };
-    },
     data() {
         return {
             loading: true,
